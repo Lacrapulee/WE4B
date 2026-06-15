19b6fd7392325bc6594926d506fa251989f90438
Linguiny01 <raphael.munch@utbm.fr>
2026-06-15 15:13:33 +0200

    Ajout de photo sur mongoDB

--- Back-end/src/includes/mongo.php
+++ Back-end/src/includes/mongo.php
@@ -5,6 +5,7 @@ use MongoDB\BSON\UTCDateTime;
 
 // On initialise la variable à null pour éviter les erreurs "Undefined variable" plus tard
 $logCollection = null;
+$imageCollection = null;
 
 try {
     // 2. Connexion
@@ -13,7 +14,7 @@ try {
     // 3. Sélection de la base de données et de la collection
     $db = $mongoClient->selectDatabase('WE4ADB_logs');
     $logCollection = $db->selectCollection('api_history');
-
+    $imageCollection = $db->selectCollection('images');
 } catch (\Exception $e) {
     // En production, il vaut mieux écrire ça dans un fichier de log classique (error_log)
     // pour éviter d'afficher tes identifiants ou des détails techniques aux utilisateurs de l'API.
